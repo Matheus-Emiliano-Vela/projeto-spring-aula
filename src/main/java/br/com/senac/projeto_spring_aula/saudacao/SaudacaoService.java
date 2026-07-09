5aa918b194a2d30af1c9d5407e8dbf8647ebd9da
Fernando Maciel <fernando.maciel@prof.sc.senac.br>
2026-07-09 18:50:28 -0300

    Iniciando com spring

---
+++ src/main/java/br/com/senac/projeto_spring_aula/saudacao/SaudacaoService.java
@@ -0,0 +1,15 @@
+package br.com.senac.projeto_spring_aula.saudacao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SaudacaoService {
+
+    private final Saudacao saudacao;
+
+    public String executar() {
+        return saudacao.mensagem();
+    }
+}
