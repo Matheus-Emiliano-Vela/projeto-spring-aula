f8bd4b96e0cf7e90173e8c665d5c06b60a9f7347
Fernando Maciel <fernando.maciel@prof.sc.senac.br>
2026-07-09 19:34:51 -0300

    Post

--- src/main/java/br/com/senac/projeto_spring_aula/todolist/ToDoListController.java
+++ src/main/java/br/com/senac/projeto_spring_aula/todolist/ToDoListController.java
@@ -1,10 +1,28 @@
 package br.com.senac.projeto_spring_aula.todolist;
 
+import br.com.senac.projeto_spring_aula.todolist.model.ListaEntity;
+import br.com.senac.projeto_spring_aula.todolist.model.ListaPostDto;
+import br.com.senac.projeto_spring_aula.todolist.model.ListaStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lista")
+@RequiredArgsConstructor
 public class ToDoListController {
 
+    private final ListaRepository listaRepository;
+
+    @PostMapping
+    public ListaEntity createTask(@RequestBody ListaPostDto dto){
+        ListaEntity listaEntity = new ListaEntity();
+
+        listaEntity.setDescricao(dto.descricao());
+        listaEntity.setStatus(ListaStatus.TO_DO);
+
+        return listaRepository.save(listaEntity);
+    }
 }
---
+++ src/main/java/br/com/senac/projeto_spring_aula/todolist/model/ListaPostDto.java
@@ -0,0 +1,6 @@
+package br.com.senac.projeto_spring_aula.todolist.model;
+
+public record ListaPostDto(
+        String descricao
+) {
+}
